5f53c977c1673c9e073c985652ac98af1a96355f
pharaoh1205 <momo1205.968@gmail.com>
2026-07-23 10:25:27 +0900

    １ファイル１クラスにクラスを分割、実行処理コードはmain.phpへ移動

---
+++ Quiz_parent.php
@@ -0,0 +1,38 @@
+<?php
+namespace PHP;
+//親のクラス：すべてのアイテムの「基本形」・Original
+class Item
+{
+    public $price;
+    public $name;
+
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+}
+
+$cider = new Drink('cider', 100);
+$cola = new Drink('cola', 150);
+
+$vendingMachine = new VendingMachine('サントリー');
+// echo $vendingMachine->pressManufacturerName();
+
+$vendingMachine->depositCoin(100);
+$vendingMachine->depositCoin(100);
+echo $vendingMachine->pressButton($cider) . "\n";
+
+// サンプル呼び出しのように、'hot' や 'ice' だけを渡してインスタンスを作る
+$hotCupCoffee = new CupCoffee('hot');
+$iceCupCoffee = new CupCoffee('ice');
+
+//カップを1つ補給する
+$vendingMachine->addCup(1);
+
+// 再度カップコーヒーを購入（カップがあるので買える！100円消費 / 残金0円）
+echo $vendingMachine->pressButton($hotCupCoffee) . "\n"; // 出力: hot cup coffee
+
+$potetoChips = new Snack("potetoChips", 150);
+$vendingMachine->depositCoin(150);
+echo $vendingMachine->pressButton($potetoChips) . "\n";
